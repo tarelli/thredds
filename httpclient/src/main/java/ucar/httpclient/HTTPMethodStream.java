dfef2b41cedd750810f11feb877813c11824c18c
dmh <dmh@unidata.ucar.edu>
2014-02-18 13:21:34 -0700

    make HttpMethodStream public

--- httpclient/src/main/java/ucar/httpclient/HTTPMethodStream.java
+++ httpclient/src/main/java/ucar/httpclient/HTTPMethodStream.java
@@ -57,7 +57,7 @@ import java.io.InputStream;
  */
 
 
-class HTTPMethodStream extends FilterInputStream
+public class HTTPMethodStream extends FilterInputStream
 {
     //////////////////////////////////////////////////////////////////////////
     static public org.slf4j.Logger log = HTTPSession.log;
